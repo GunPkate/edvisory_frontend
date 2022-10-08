72377aa3397ac7ca369a168eac4fb4a78cd62f31
GunPKate <gun@builk.com>
2022-10-08 16:40:16 +0700

    first react

---
+++ src/components/Note.js
@@ -0,0 +1,11 @@
+const Note = ()=>{
+    return <div className="note">
+        <span>Yes!!</span>
+        <div className="note-footer">
+            <small>13/01/2022</small>
+            {/* <MdDeleteForever className='delete-icon' size="1.3em"/>  */}
+        </div>   
+    </div>
+}
+
+export default Note
---
+++ src/components/NotesList.js
@@ -0,0 +1,13 @@
+import Note from "./Note"
+
+const NoteList =() =>{
+    return(
+        <div className="notes-list">
+            <Note/>
+            <Note/>
+            <Note/>
+        </div>
+    )
+}
+
+export default NoteList
